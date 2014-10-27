e9fb2810fc4881f6143f8be15df423043230c5b3
Tobbe <tobbejonsson1984@hotmail.com>
2014-10-27 09:56:11 +0100

    Chess

---
+++ Chess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fun chess game is fun");
+            Console.ReadLine();
+        }
+    }
+}
